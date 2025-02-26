9394c89c429dacc63e3e98b9b58477b211a4ff99
LeAlanEnVrai <alan.runavot@gmail.com>
2025-02-26 10:49:09 +0100

    ibezg

--- site_internet/app.js
+++ site_internet/app.js
@@ -6,3 +6,32 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+    return res.render('index');
+});
+
+app.get('/prestations', async (req, res) => {
+    return res.render('prestation');
+});
+
+app.get('/partenaires', async (req, res) => {
+    return res.render('partenaires');
+});
+
+app.get('/projets', async (req, res) => {
+    return res.render('projets');
+});
+
+app.get('/rendez-vous', async (req, res) => {
+    return res.render('rdv');
+});
+
+app.get('/contact', async (req, res) => {
+    return res.render('contact');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+}
+)
